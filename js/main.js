effb41470a69ac4d77f907e5c93bc54e5f7c6dc4
kevintudiep <kevintudiep@gmail.com>
2018-10-31 15:51:52 -0500

    added eventlistener to button, changed button status, body colour, and text status

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
 
+var button = document.querySelector(".switch.on");
+var status = document.querySelector(".status")
+var body = document.querySelector(".light")
 
+console.log(button.className);
+
+button.addEventListener('click', function() {
+  if (button.className === "switch on") {
+    button.className = "switch off"
+    body.className = "dark"
+    status.innerText = "Where did the lights go?"
+  } else {
+  button.className = "switch on"
+  body.className = "light"
+  status.innerText = "It's so bright in here!"
+}
+});
+
+})
